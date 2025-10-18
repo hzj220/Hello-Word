8510f7066fd45e6bd0bcb63c81b5469ca3d1e440
hzj220 <huzhijin2025@163.com>
2025-10-18 17:42:01 +0800

    sort smaller to biggermain.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[],int size){
+	int temp=0;
+	for(int i=0;i<size-1;i++){
+		for(int j=0;j<size-1-i;j++){
+			if(arr[j]>arr[j+1]){
+			 temp=arr[j+1];
+			 arr[j+1]=arr[j];
+			 arr[j]=temp;
+			}
+		}
+	}
+}
 int main(){
-    cout<<"Hello world!"<<end;
-    return 0;
+	int arr[]={50,40,30,20,10,0};
+	int size=sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+         return 0;
 }
